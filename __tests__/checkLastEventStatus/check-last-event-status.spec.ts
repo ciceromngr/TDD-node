4b4e17c3520f0086d0c7f8c834420f22de63858f
Cicero Romao <cicerotransformice@gmail.com>
2021-12-27 06:14:26 -0300

    ensure CheckLastEventStatus return status done when now is after to review time

--- __tests__/checkLastEventStatus/check-last-event-status.spec.ts
+++ __tests__/checkLastEventStatus/check-last-event-status.spec.ts
@@ -9,10 +9,18 @@ class CheckLastEventStatus {
     ) { }
 
     async perform({ groupId }: { groupId: string }): Promise<EventStatus> {
+
         const event = await this.loadLastEventRepository.loadLastEvent({ groupId })
         if (event === undefined) return { status: 'done' }
+        
         const now = new Date()
-        return event.endDate >= now ? { status: 'active' } : { status: 'inReview' }
+        if (event.endDate >= now) return { status: 'active' }
+
+        const reviewDurationInMs = event.reviewDurationInHours * 60 * 60 * 1000
+        const reviewDate = new Date(event.endDate.getTime() + reviewDurationInMs)
+
+        return reviewDate >= now ? { status: 'inReview' } : { status: 'done' }
+
     }
 
 }
@@ -134,6 +142,20 @@ describe('CheckLastEventStatus', () => {
         }
         const eventStatus = await sut.perform({ groupId })
         expect(eventStatus.status).toBe('inReview')
+
+    })
+
+    it('should return status done when now is after to review time', async () => {
+
+        const reviewDurationInHours = 1
+        const reviewDurationInMs = reviewDurationInHours * 60 * 60 * 1000
+        const { sut, loadLastEventRepository } = makeSut()
+        loadLastEventRepository.output = {
+            endDate: new Date(new Date().getTime() - reviewDurationInMs - 1),
+            reviewDurationInHours
+        }
+        const eventStatus = await sut.perform({ groupId })
+        expect(eventStatus.status).toBe('done')
 
     })
 })
